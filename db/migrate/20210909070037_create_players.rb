4fb4cf2069f7b063ca466069227a45cf69ca3ba6
nurbrun <the.nurbrun@gmail.com>
2021-09-09 03:52:58 -0400

    added league and player models and migrations. added seed file. moved json file. added pry to dev environment

---
+++ db/migrate/20210909070037_create_players.rb
@@ -0,0 +1,23 @@
+class CreatePlayers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :players do |t|
+      t.references :league
+      t.string :player
+      t.string :team
+      t.string :pos
+      t.integer :att
+      t.float :attg
+      t.integer :yds
+      t.float :avg
+      t.float :ydsg
+      t.integer :td
+      t.string :lng
+      t.integer :first
+      t.float :first_percent
+      t.integer :twenty_plus
+      t.integer :forty_plus
+      t.integer :fum
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if League.count === 0
+  puts "No leagues found in database. NFL is required for this example."
+  league = League.create!(league_name: "NFL")
+  puts "Created #{league.league_name}"
+end
+
+if Player.count === 0
+  puts "There are no players in the database"
+  league = League.find_by_league_name("NFL")
+  path = File.join(File.dirname(__FILE__), "./data/rushing.json")
+  players = JSON.parse(File.read(path))
+  players.each do |player|
+    Player.create!(
+       player: player["Player"],
+       team: player["Team"],
+       pos: player["Pos"],
+       att: player["Att"],
+       attg: player["Att/G"],
+       yds: player["Yds"],
+       avg: player["Avg"],
+       ydsg: player["Yds/G"],
+       td: player["TD"],
+       lng: player["Lng"],
+       first: player["1st"],
+       first_percent: player["1st%"],
+       twenty_plus: player["20+"],
+       forty_plus: player["40+"],
+       fum: player["FUM"],
+       league_id: league.id
+      )
+  end
+  puts "Players seeded: #{Player.count}"
+end
