1da920c54ded4e6134d804dfa5110ac030f26557
nurbrun <the.nurbrun@gmail.com>
2021-09-09 06:43:25 -0400

    added simple player table. added sort. added simple sanitization. added csv download

--- app/controllers/nfl_controller.rb
+++ app/controllers/nfl_controller.rb
@@ -1,7 +1,21 @@
 class NflController < ApplicationController
 
   def rushing
-
+    sort_params = params[:sort]
+    dir_params = params[:dir]
+    permitted_sorts = ["yds","td","lng","ASC","DESC"]
+    sanitized_sort_params = permitted_sorts.include?(sort_params) ? sort_params : nil
+    sanitized_dir_params = permitted_sorts.include?(dir_params) ? dir_params : "ASC"
+    nfl_id = League.find_by_league_name("NFL")
+    if sanitized_sort_params === nil
+      @players = Player.where(:league_id => nfl_id)
+    else
+      @players = Player.where(:league_id => nfl_id).order("#{sanitized_sort_params} #{sanitized_dir_params}")
+    end
+    respond_to do |format|
+      format.html
+      format.csv { send_data @players.to_csv, filename: "theRush-#{Date.today}.csv" }
+    end
   end
 
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,15 @@
+require 'csv'
 class Player < ApplicationRecord
   belongs_to :league
-  
 
+  def self.to_csv
+    columns_to_filter = ["created_at", "updated_at", "league_id", "id"]
+    attributes = Player.column_names - ["created_at", "updated_at", "league_id", "id"] - columns_to_filter
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+      all.each do |user|
+        csv << attributes.map{ |attr| user.send(attr) }
+      end
+    end
+  end
 end
