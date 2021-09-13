c256bad5bb45562267ce0e5163cf382c7d7c9632
nurbrun <the.nurbrun@gmail.com>
2021-09-12 22:50:44 -0400

    added partial match serach. table now remebers direction between sorts. added strong params.  player query logic now includes player filter.

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,14 +2,21 @@ require 'csv'
 class Player < ApplicationRecord
   belongs_to :league
 
-  def self.to_csv
+  def self.to_csv(players)
     columns_to_filter = ["created_at", "updated_at", "league_id", "id"]
     attributes = Player.column_names - ["created_at", "updated_at", "league_id", "id"] - columns_to_filter
     CSV.generate(headers: true) do |csv|
       csv << attributes
-      all.each do |user|
-        csv << attributes.map{ |attr| user.send(attr) }
+      players.each do |player|
+        csv << attributes.map{ |attr| player.send(attr) }
       end
     end
   end
+
+  def search_players
+    if @player = Player.all.find{|player| player.name.include?(params[:search])}
+      redirect_to player_path(@player)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get '/rushing', to: 'nfl#rushing'
+get '/rushing', to: 'nfl#rushing', as: 'players'
 root 'nfl#rushing'
 
 end
